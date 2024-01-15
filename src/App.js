750d899e0fc1142b1676980bc0c6355a2ba4aa34
kajiman <kajiman@waterflowtechnology.net>
2024-01-15 11:12:21 +0545

    chore: update cod eto handle the updated skills and project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import { About,Footer,Header,Skills,Testimonial,Work } from './components';
-import Navbar from './container/Navbar/Navbar';
-import './App.scss';
-import Practice from './components/Practice/Practice';
-
+import React from "react";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./components";
+import Navbar from "./container/Navbar/Navbar";
+import "./App.scss";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/> 
-      <Header/> 
-      <About/>
-      <Work/>
-      <Skills/>
-      <Testimonial/>
-      <Footer/>
-      <Practice/>
-
-
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
-}
+};
 
-export default App
+export default App;
