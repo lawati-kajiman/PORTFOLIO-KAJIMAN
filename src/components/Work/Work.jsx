d6750e854cb124b212ed05ce5da86c0af788ba26
kajiman <kajiman@waterflowtechnology.net>
2024-01-15 11:10:59 +0545

    chore: This is the work section of my portfolio

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,29 +1,58 @@
-import React, { useState} from 'react';
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
-import { motion } from 'framer-motion';
-import { images } from '../../constants';
+import React, { useState } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 
-import { AppWrap,MotionWrap} from '../../wrapper';
-import './Work.scss';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./Work.scss";
 
-const works=[
+const works = [
   {
-    title:"Progressive web app",description:"Made an pwa web app as project for my college assignment ",imgUrl: images.adidas,projectLink:"",codeLink:"",tags:"Web App"
+    title: "Progressive web app",
+    description: "Made an pwa web app as project for my college assignment ",
+    imgUrl: images.adidas,
+    projectLink: "",
+    codeLink: "",
+    tags: "Web App",
   },
   {
-    title:"Bolt",description:"fully functional app for shopping and customizing ", imgUrl:images.bolt,projectLink:"",codeLink:"",tags:"Mobile App"
+    title: "Numayou",
+    description: "fully functional app for shopping and customizing ",
+    imgUrl: images.bolt,
+    projectLink: "",
+    codeLink: "",
+    tags: "Web App",
   },
   {
-    title:"Portfolio",description:"Portfolio is a fully functional website ",imgUrl:images.about03,projectLink:"",codeLink:"",tags:"React JS"
+    title: "Portfolio",
+    description: "Portfolio is a fully functional website ",
+    imgUrl: images.about03,
+    projectLink: "",
+    codeLink: "",
+    tags: "React JS",
   },
   {
-    title:"Numayou",description:"Developing app will always be a good thing for me and having a good design is like a cherry on pie .",imgUrl:images.amazon,projectLink:"",codeLink:"",tags:"UI/UX"
-  } 
-]
+    title: "Trade Flow",
+    description:
+      "Developing app will always be a good thing for me and having a good design is like a cherry on pie .",
+    imgUrl: images.amazon,
+    projectLink: "",
+    codeLink: "",
+    tags: "Web App",
+  },
+  {
+    title: "",
+    description: "fully functional app for shopping and customizing ",
+    imgUrl: images.bolt,
+    projectLink: "",
+    codeLink: "",
+    tags: "UI/UX",
+  },
+];
 
 const Work = () => {
   const [filterWork, setFilterWork] = useState([]);
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   const handleWorkFilter = (item) => {
@@ -33,7 +62,7 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
@@ -43,55 +72,57 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> Section
+      </h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
-          <div
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-          >
-            {item}
-          </div>
-        ))}
+        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}>
+              {item}
+            </div>
+          )
+        )}
       </div>
 
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__work-portfolio"
-      >
+        className="app__work-portfolio">
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
-            <div
-              className="app__work-img app__flex"
-            >
+            <div className="app__work-img app__flex">
               <img src={work.imgUrl} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex"
-              >
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex">
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
-
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
+                    className="app__flex">
                     <AiFillEye />
                   </motion.div>
                 </a>
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
+                    className="app__flex">
                     <AiFillGithub />
                   </motion.div>
                 </a>
@@ -100,7 +131,9 @@ const Work = () => {
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags}</p>
@@ -113,5 +146,7 @@ const Work = () => {
   );
 };
 export default AppWrap(
-  MotionWrap(Work,'app__works'),
-'work',"app__primarybg");
+  MotionWrap(Work, "app__works"),
+  "work",
+  "app__primarybg"
+);
