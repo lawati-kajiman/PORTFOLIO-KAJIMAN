4f09d1913661b2660a3c78a12ffaf23f44ae4872
kajiman <kajiman@waterflowtechnology.net>
2024-01-15 11:11:48 +0545

    info: This is the about  section of my portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,42 +1,55 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import './About.scss';
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { images } from '../../constants';
+import React from "react";
+import { motion } from "framer-motion";
+import "./About.scss";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { images } from "../../constants";
 
-const abouts=[
+const abouts = [
   {
-    title:"Web Development",description:"I am a good Web Developer.",imgUrl: images.about01
+    title: "Web Development",
+    description: "I am a good Web Developer.",
+    imgUrl: images.about01,
   },
   {
-    title:"UI/UX",description:"I am a good ui/ux designer.", imgUrl:images.about02
+    title: "UI/UX",
+    description: "I am a good ui/ux designer.",
+    imgUrl: images.about02,
   },
   {
-    title:"Graphic design",description:"I have quite good experience in graphic design.",imgUrl:images.about03
+    title: "Figma",
+    description: "I have quite good experience in graphic design.",
+    imgUrl: images.about03,
   },
   {
-    title:"Flutter",description:"Developing app will always be a good thing for me .",imgUrl:images.about04
-  } 
-]
+    title: ".Net Backend Developer",
+    description: "Developing app will always be a good thing for me .",
+    imgUrl: images.about04,
+  },
+];
 
 const About = () => {
-
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+      <h2 className="head-text">
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
+            transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+            key={about.title + index}>
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
@@ -45,5 +58,7 @@ const About = () => {
 };
 
 export default AppWrap(
-  MotionWrap(About,'app__about'),
-'about',"app__whitebg");
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
